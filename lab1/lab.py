c2c7777ec38d0061330cd92c43501702502ce23b
Andrei Tcibin <Andrei_Tcibin@epam.com>
2019-02-17 13:04:10 +0300

    Добавляет решение первой лабораторной работы

---
+++ lab1/lab.py
@@ -0,0 +1,87 @@
+import logging
+from operator import concat
+from functools import reduce
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from common.gen import LinearCongruentialGenerator
+from common.log import init_logging
+from lab1.markov import MarkovChain
+
+init_logging()
+
+
+def show_transition_diagrams(models, corner_states):
+    fig, axes = plt.subplots(2, 2)
+    for index, model in enumerate(models):
+        ax = axes[int(index / 2), index % 2]
+        ax.plot(model.transitions, drawstyle='steps')
+        ax.set_ylim(corner_states)
+        ax.set_title('Initial state: %s' % model.initial_state)
+    plt.show()
+
+
+def main():
+    # Матрица переходных состояний
+    G = [
+        [0.3, 0.3, 0, 0.4],
+        [0.7, 0.3, 0, 0],
+        [0, 0, 0.4, 0.6],
+        [0, 0, 0.2, 0.8]
+    ]
+
+    # Конфигурации
+    states = range(0, 4)
+    modelling_steps = 200
+    models_number = 10
+    initial_values = (i for i in [42352531])
+    numbers_after_dot = 2
+
+    # Генераторы случайных величин
+    lcg = LinearCongruentialGenerator(initial_values.__next__())
+
+    # 2. Прогоны модели с начальными значениями, соответствующими состояниям дискретной модели.
+    initial_models = [MarkovChain(G, lcg, state) for state in states]
+
+    for index, model in enumerate(initial_models):
+        logging.info('%s/%s models has finished.' % (index + 1, len(initial_models)))
+        model.model(modelling_steps)
+
+    # 3. Отображение диаграмм состояния дискретных марковских цепей.
+    show_transition_diagrams(initial_models, [states[0] - 1, states[-1] + 1])
+
+    # 4. Определение видов состояний марковской цепи, классов эквивалентных состояний.
+    #    1 состояние - несущественное, нулевое, возвратное, непериодическое.
+    #    2 состояние - несущественное, нулевое, возвратное, непериодическое.
+    #    3 состояние - существенное, положительное, возвратное, непериодическое.
+    #    4 состояние - существенное, положительное, возвратное, непериодическое.
+    #    3 и 4 - сообщающиеся состояния.
+
+    # 5. Эксперименты для определения стационарного распределения вероятностей дискретной марковской цепи.
+    models = ([MarkovChain(G, lcg, state) for state in states] * (int(models_number / len(states)) + 1))[:models_number]
+    for index, model in enumerate(models):
+        logging.info('%s/%s models has finished.' % (index + 1, len(models)))
+        model.model(modelling_steps)
+
+    all_transitions = reduce(concat, [model.transitions for model in models])
+
+    stationary_probabilities = [all_transitions.count(state) / len(all_transitions) for state in states]
+    logging.info('Experiments stationary probabilities = %s.'
+                 % list(np.around(stationary_probabilities, numbers_after_dot)))
+
+    # 6. Расчет стационарного распределения вероятностей дискретной марковской цепи.
+    mG = []
+    for state in states:
+        mg = [g[state] for g in G]
+        mg[state] -= 1
+        mG.append(mg)
+    mG[-1] = [1, 1, 1, 1]
+    a = np.array(mG)
+    b = np.array([0, 0, 0, 1])
+    x = np.linalg.solve(a, b)
+    logging.info('Calculated stationary probabilities = %s.' % list(np.around(x, numbers_after_dot)))
+
+
+if __name__ == '__main__':
+    main()
---
+++ lab1/markov.py
@@ -0,0 +1,30 @@
+import logging
+
+
+class MarkovChain:
+
+    def __init__(self, G, lcg, initial_state):
+        self.G = G
+        self.lcg = lcg
+        self.initial_state = initial_state
+        self.state = self.initial_state
+        self.transitions = [self.state]
+
+    def model(self, num_steps):
+        for step in range(num_steps):
+            transitions = self.G[self.state]
+            available_transitions = [(prob, state) for (state, prob) in enumerate(transitions) if prob > 0]
+            self.state = self._get_next_state(available_transitions)
+            self.transitions.append(self.state)
+
+    def _get_next_state(self, available_transitions):
+        generated_prob = self.lcg.__next__()
+        collected_prob = 0
+        for (prob, state) in available_transitions:
+            collected_prob += prob
+            if collected_prob > generated_prob:
+                return state
+        logging.warning('Taking the last available state because collected probability differs '
+                        'with generated one:\n%s\n%s'
+                        % (collected_prob, generated_prob))
+        return available_transitions[:-1][1]
